8b4bbd9d79a47dd512c55f339fb48d8c3007b379
manucho254 <manuchoadero@gmail.com>
2021-10-25 16:57:21 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -3,33 +3,38 @@
 // we will take care of the api soon
 const APIMOVIEURL =  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=';
 const APITVSERIESURL = 'https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=';
-const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHMOVIEAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 const SEARCHSERIESAPI = 'https://api.themoviedb.org/3/search/tv?&api_key=04c35731a5ee918f014970082a0088b1&query='
 const iframeMovieLink = "https://autoembed.xyz/movie/tmdb/";
 const iframeSeriesLink = "https://autoembed.xyz/tv/tmdb/";
 const TvSeriesurl = "https://api.themoviedb.org/3/tv/"
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const apiKey = "?api_key=04c35731a5ee918f014970082a0088b1&language=en-US";
 
 // getting some of my elements
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+
+const selectSeriesEpesisode = document.createElement("select");
+const playOverview = document.getElementById("play-overview");
+const paginated = document.getElementById("pagination");
+const selectSeason = document.createElement("select");
+const playContentData = document.createElement("div");
 const tvSeries = document.getElementById("tvSeries");
+const search = document.getElementById("search");
 const movies = document.getElementById("movies");
 const nextPage = document.getElementById("next");
 const prevPage = document.getElementById("prev");
-const paginated = document.getElementById("pagination");
-const playOverview = document.getElementById("play-overview");
-const playContentData = document.createElement("div");
-const iframe = document.createElement("div");
 const movieTitle = document.createElement("div");
-const selectSeason = document.createElement("select");
-const selectSeriesEpesisode = document.createElement("select");
-selectSeason.classList.add('select-dropdown');
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const iframe = document.createElement("div");
+
 selectSeriesEpesisode.classList.add("select-dropdown");
+selectSeason.classList.add('select-dropdown');
+iframe.classList.add("iframeSizing");
 movieTitle.classList.add("iframes");
 
-let image = "empty.jpg";
+let image = "empty.jpg"; // this is for movies that dont have images
+
 
 getTvSeries(APITVSERIESURL);
 getMovies(APIMOVIEURL);
@@ -40,6 +45,8 @@ tvSeries.addEventListener("click",  () => {
     getTvSeries(APITVSERIESURL)
     main.classList.add("series")
     main.classList.remove("movies")
+    playOverview.innerHTML = "";
+    main.appendChild(paginated);
 })
 
 
@@ -49,6 +56,8 @@ movies.addEventListener("click",  () => {
     getMovies(APIMOVIEURL)
     main.classList.add("movies")
     main.classList.remove("series")
+    playOverview.innerHTML = "";
+    main.appendChild(paginated);
 })
 
 // fetch the url and get the movies info objects
@@ -63,8 +72,6 @@ async function getTvSeries(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
     showTvSeries(respData.results)
-    console.log(respData.results);
-
 }
 
 // get all Movies
@@ -72,8 +79,8 @@ async function getTvSeries(url) {
 function showMovies(movies) {
 
     main.innerHTML = '';
-
-    movies.forEach((movie) => {
+    // mapping all the objects to geta single object can also forEach
+    movies.map((movie) => {
 
         const {poster_path,title,vote_average,overview, release_date, id} = movie
         const movieEl = document.createElement("div");
@@ -94,7 +101,7 @@ function showMovies(movies) {
             </div>`
         }else {
             movieEl.innerHTML = 
-            `<img src="${image.jpg}" alt="${title}">
+            `<img src="${image}" alt="${title}">
             <div class="movie-info">
             <h3>${title}</h3>
             <span class="${getClassByRate(vote_average)}">${vote_average}</span>
@@ -106,18 +113,22 @@ function showMovies(movies) {
         }
         main.appendChild(movieEl)
         movieEl.addEventListener("click", () => {
-
+            paginated.remove();
             movieTitle.innerText = `${title}`;
-            playContentData.innerHTML = `<span>${title}</span><br><span>Release Date: ${ release_date }</span><br><span class="${getClassByRate(vote_average)}">Rating : ${vote_average}</span><br><span>Overview: <p>${overview}</p></span>`
+            playContentData.innerHTML = `
+            <div class="play-images">
+              <img src="${IMGPATH + poster_path}" alt="${title}">
+            </div>
+            <span>${title}</span><br><br><span>Release Date: ${ release_date }</span>
+            <br><br><span class="${getClassByRate(vote_average)}">Rating : ${vote_average}
+            </span><br><span>Overview: <p>${overview}</p></span>`
             iframe.innerHTML = 
-                   `<iframe src="${iframeMovieLink + id}" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">`
+                `<iframe src="${iframeMovieLink + id}" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">`
                  
             main.innerHTML = '';
             main.appendChild(movieTitle);
             main.appendChild(iframe);
             playOverview.appendChild(playContentData);
-            paginated.removeChild(nextPage);
-            paginated.removeChild(prevPage);
         })
     });
     
@@ -129,8 +140,8 @@ function showTvSeries(movies) {
 
     main.innerHTML = '';
 
-    // looping through all the tv shows data using 
-    movies.forEach((movie) => {
+    // looping through all the tv shows data using map can also use forEach
+    movies.map((movie) => {
 
         const {poster_path,name,vote_average, overview, first_air_date, id} = movie;
         const movieEl = document.createElement("div");
@@ -139,23 +150,23 @@ function showTvSeries(movies) {
         if (poster_path != null) {
             movieEl.innerHTML = 
             `<span class="year">${first_air_date}</span>
-            <img src="${IMGPATH + poster_path}" alt="${name}">
-            <div class="movie-info">
-            <h3>${name}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-            <div class="overview">
+             <img src="${IMGPATH + poster_path}" alt="${name}">
+             <div class="movie-info">
+              <h3>${name}</h3>
+              <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+             </div>
+             <div class="overview">
               <button class="trailer" id="allTrailers">Play : &#9654;</button>
                <h3>Overview: </h3>
-                 ${overview}
-            </div>`
+                ${overview}
+             </div>`
 
         }else {
             movieEl.innerHTML = 
             `<img src="${image}" alt="${name}">
             <div class="movie-info">
-            <h3>${name}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+              <h3>${name}</h3>
+              <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
               <h3>Overview: </h3>
@@ -171,12 +182,21 @@ function showTvSeries(movies) {
             //getting the episodes for every season
             selectSeason.innerHTML = '';
             main.innerHTML = '';
-            const tvSeriesSeason = `${TvSeriesurl + id}?api_key=04c35731a5ee918f014970082a0088b1&language=en-US`       
+            const tvSeriesSeason = `${TvSeriesurl + id + apiKey}`       
             movieTitle.innerText = `${name}`;
-            playContentData.innerHTML = `<span>${name}</span><br><span>Release Date: ${ first_air_date }</span><br><span class="${getClassByRate(vote_average)}">Rating : ${vote_average}</span><br><span>Overview: <p>${overview}</p></span>`;
+            playContentData.innerHTML = `
+            <div class="play-images">
+             <img src="${IMGPATH + poster_path}" alt="${name}">
+            </div>
+             <span>${name}</span><br><br><span>Release Date: ${ first_air_date }</span>
+             <br><br><span class="${getClassByRate(vote_average)}">Rating : ${vote_average}</span>
+             <br><br><span>Overview: <p>${overview}</p></span>`;
             
             async function getSeason() {
-                iframe.innerHTML = `<iframe src="${iframeSeriesLink + id + "-" + season + "-" + episode}" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">`
+                paginated.remove();
+                iframe.innerHTML = `
+                 <iframe src="${iframeSeriesLink + id + "-" + season + "-" + episode}" 
+                  frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">`
                 const resp = await fetch(tvSeriesSeason);
                 const respData = await resp.json();
         
@@ -184,55 +204,48 @@ function showTvSeries(movies) {
                     const option = document.createElement("option");
                     selectSeason.classList.add("active");
                     option.value = `${index}`;
-                    option.innerText = `${index}`;
+                    option.innerText = `Season ${index}`;
                     selectSeason.appendChild(option);
                     main.appendChild(selectSeason);
-                    let CountLoadEpisodesClicks  = 0;
 
                     option.addEventListener("click", () => {
-                        CountLoadEpisodesClicks += 1; // checking the number clicks for a certain season to load Episodes 
                         season = option.value;
-                        iframe.innerHTML = `<iframe src="${iframeSeriesLink + id + "-" + season + "-" + episode}" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">`;
+                        iframe.innerHTML = `
+                          <iframe src="${iframeSeriesLink + id + "-" + season + "-" + episode}" 
+                          frameborder="0" scrolling="no" allowfullscreen="allowfullscreen"`;
+
                         async function getSeasonEpisodes() {
-                            const tvSeriesEpisodes = `${TvSeriesurl + id}/season/${season}?api_key=04c35731a5ee918f014970082a0088b1&language=en-US`
+                            const tvSeriesEpisodes = `${TvSeriesurl + id}/season/${season + apiKey}`;
                             const resp = await fetch(tvSeriesEpisodes);
                             const respData = await resp.json();
-                            console.log(respData);
                             // iterating through the Episodes and creating a button
-                           
+                            
                             for (index = 1 ; index <= respData.episodes.length; index++){
                                 const option = document.createElement("option");
-
-                                option.innerText = `${index}`;
+                                option.innerText = `Episode ${index}`;
                                 option.value = `${index}`;
-
                                 selectSeriesEpesisode.appendChild(option);
+                                console.log(selectSeriesEpesisode)
                                 main.appendChild(selectSeriesEpesisode);
                                 option.addEventListener("click", () => { 
                                     episode = option.value;
-                                    iframe.innerHTML = `<iframe src="${iframeSeriesLink + id + "-" + season + "-" + episode}" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">` 
+                                    iframe.innerHTML = `
+                                     <iframe src="${iframeSeriesLink + id + "-" + season + "-" + episode}" 
+                                     frameborder="0" scrolling="no" allowfullscreen="allowfullscreen" >` 
                                 }); 
                             }
                         };
-                        if (CountLoadEpisodesClicks == 1) {
-                            getSeasonEpisodes()
-                            selectSeriesEpesisode.innerHTML = "";
-                            main.removeChild(selectSeriesEpesisode);
-                            main.appendChild(selectSeriesEpesisode);
-                        }else {
-                            console.log("error")
-                        }
+                        getSeasonEpisodes()
+                        selectSeriesEpesisode.innerHTML = "";
+                        main.removeChild(selectSeriesEpesisode);
+                        main.appendChild(selectSeriesEpesisode);
                     }); 
                 };
             };
             getSeason();
             main.appendChild(movieTitle);
             main.appendChild(iframe);
-            console.log(iframe)
             playOverview.appendChild(playContentData);
-            // paginated.removeChild(nextPage);
-            // paginated.removeChild(prevPage);  
-            paginated.remove()        
         })
     });
 }
@@ -248,22 +261,29 @@ function getClassByRate(vote) {
     }
 }
 
+let searchData = [];
 // Search fuctionality but not the best, but as of now we will let it be
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const searchTerm = search.value;
 
     if (searchTerm){
-        getTvSeries(SEARCHSERIESAPI + searchTerm);
-        getMovies(SEARCHMOVIEAPI + searchTerm);
+        if (main.classList == "series") {
+            getTvSeries(SEARCHSERIESAPI + searchTerm);
+        }
+        else if (main.classList == "movies") {
+            getMovies(SEARCHMOVIEAPI + searchTerm);
+
+        }else {
+            getTvSeries(SEARCHSERIESAPI + searchTerm);
+            getMovies(SEARCHMOVIEAPI + searchTerm);
+        }
         search.value = '';
-        playOverview.remove();
-        
     }
 });
 
 // pagination using button but planning to do an infinate scroll
+
 
 let index = 1;
 
@@ -271,7 +291,7 @@ nextPage.addEventListener("click", () => {
     index++;
     if (main.classList == "series") {
         getTvSeries(APITVSERIESURL + index);
-    }else{
+    }else if (main.classList == "movies" || main.classList == "") {
         getMovies(APIMOVIEURL + index);
     }
 });
